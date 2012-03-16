896d57e3790a1300086ac795ee96cfdbf2f724e3
Jordan Byron <jordan.byron@gmail.com>
2012-03-16 14:03:17 -0600

    Remove additional insurance questions

--- db/migrate/20120316040144_update_survey_for_2012.rb
+++ db/migrate/20120316040144_update_survey_for_2012.rb
@@ -1,9 +1,13 @@
 class UpdateSurveyFor2012 < ActiveRecord::Migration
   def up
     remove_column :surveys, :husky_insurance
+    remove_column :surveys, :saga_insurance
+    remove_column :surveys, :medicaid_or_chp_plus
+
     %w{a b c d unknown}.each do |husky_type|
       add_column :surveys, "husky_insurance_#{husky_type}", :boolean
     end
+
     add_column :surveys, :charter_oak, :boolean
   end
 
