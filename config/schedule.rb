97e2c58679bf1136a394052c5420a64e822b4696
Jordan Byron <jordan.byron@gmail.com>
2012-03-22 18:32:50 -0400

    Update backup script

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,8 +1,5 @@
 set :output, "/home/deploy/mom/current/log/cron.log"
 
 every 15.minutes do
-  db_config  = YAML.load_file("#{Dir.pwd}/config/database.yml")["production"]
-  mom_config = YAML.load_file("#{Dir.pwd}/config/mom.yml")
-
-  command %{PGPASSWORD=#{db_config["password"].inspect} pg_dump -i -h #{db_config["host"]} -U #{db_config["username"]} -F c -f "#{mom_config["backup_path"]}`date \\+\\%m_\\%d_\\%Y_\\%H_\\%M`.backup" #{db_config["database"]}}
+  command "backup.sh"
 end
