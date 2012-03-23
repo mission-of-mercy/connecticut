c291ca2ebd1526a1c510fcdbc1b2137482d53492
Jordan Byron <jordan.byron@gmail.com>
2012-03-23 17:19:23 -0400

    Revert "Add server time warning"
    This reverts commit 89f5b424feff6bb7f0c6bf120a5b25f299772118.

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -1,7 +1,7 @@
 class StatusController < ApplicationController
   def index
-    @requests    = SupportRequest.where(:resolved => false)
-    @patients    = Patient.where("patients.created_at::Date = ?", Date.today).count
-    @server_time = Time.now
+    @requests = SupportRequest.where(:resolved => false)
+    @patients = Patient.where("patients.created_at::Date = ?", Date.today).count
   end
+
 end
